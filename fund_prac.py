1d79ec5dcbca84530f9383e6b4007e3bea2c5a04
BenLiang11 <benliang03@gmail.com>
2021-06-16 09:39:37 -0700

    three

--- fund_prac.py
+++ fund_prac.py
@@ -1,1 +1,22 @@
 print ("Hello world")
+
+# testing print
+
+#Step 1
+name = "Bob" #string
+age = "21" #int
+salary = "90.56" #float
+plays_video_games = True #boolean
+fav_num = "56" #int
+
+#Step 2
+car_name = "Tesla"
+x = 50
+my_first_name = "Joe"
+
+print (type(name))
+print (type(age))
+print (type(salary))
+print (type(plays_video_games))
+print (type(fav_num))
+
