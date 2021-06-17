f19e417b31e04382adfb43ff170d209bbe808d10
BenLiang11 <benliang03@gmail.com>
2021-06-17 09:44:52 -0700

    list prac

---
+++ list_prac.py
@@ -0,0 +1,30 @@
+city_names = ["Oakland", "Atlanta", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "New Orleans"]
+resturaunts = ["Chipotle", "McDonalds", "Popeyes", "WingStop", "Starbucks", "Taco Bell", "Subway", "Chick-fil-A"]
+resturaunts[5] = "In-N-Out"
+
+print (resturaunts[1])
+
+#list slicing
+three_cities = city_names [0:3]
+print (three_cities)
+four_resturaunts = resturaunts [0:4]
+print (four_resturaunts)
+
+#change
+city_names [0] = "San Francisco"
+city_names [2] = "Brooklyn" 
+city_names [7] = "Hollywood" 
+city_names [5] = "Tampa"
+
+#add
+city_names.append("New Jersey")
+city_names.extend(["Santa Cruz", "Selma", "Chicago"])
+city_names.insert(7, "Washington, D.C.")
+print (city_names)
+
+#delete
+del city_names [0]
+city_names.pop (4)
+city_names.remove ("Chicago")
+print (city_names)
+
